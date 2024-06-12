830044e3ccfad0b63c97cdd6b3482e6c79573630
Kriidfel <g.mesureur@student.helmo.be>
2024-06-12 12:05:10 +0200

    feat(register): adding some register logic

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const DataBase = require ('../model/DataBase')
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
 
+    let dataUser = req.body;
+    let db = new DataBase()
+    console.log(dataUser)
+    //await db.query(`INSERT INTO Users (Pseudo, Password, Mail) VALUES ('${dataUser.Pseudo}', '${dataUser.Password}', '${dataUser.Mail}')`)
 
+    res.send("The data has been received!");
 });
 
 module.exports = router;
