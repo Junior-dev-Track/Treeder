b430a586d0cb52a28570b1a47080fb3b14a6d30a
“Lunoronti” <cacciatoremanon8@gmail.com>
2024-06-13 09:57:52 +0200

    routes

--- server/routes/register.js
+++ server/routes/register.js
@@ -7,6 +7,18 @@ router.post('/', async (req, res) => {
     let dataUser = req.body;
     let db = new DataBase()
     console.log(dataUser)
+
+    
+
+    // vérifier les injections sql / js
+    // nettoyer les espaces blancs
+    // vérifier que l'user n'existe pas déjà
+    // vérfier les mdp
+    // vérifier la force du mdp (si temps)
+    // envoyer les données 
+
+
+
     //await db.query(`INSERT INTO Users (Pseudo, Password, Mail) VALUES ('${dataUser.Pseudo}', '${dataUser.Password}', '${dataUser.Mail}')`)
 
     res.send("The data has been received!");
