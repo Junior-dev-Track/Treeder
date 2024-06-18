6696e517f54afafee95287a075d6644588db4869
Mathilde Po <mathilde@MacBook-Pro-de-Mathilde-2.local>
2024-06-18 15:07:20 +0200

    bug de m

--- client/src/components/StadiaMap.jsx
+++ client/src/components/StadiaMap.jsx
@@ -1,44 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import treeImage from '../assets/img/tree.png'; 
 
-import 'leaflet.markercluster/dist/MarkerCluster.css';
-import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
-import 'leaflet.markercluster';
-
-
-
-const MarkerClusterGroupComponent = ({ treeData, treeIcon }) => {
-  const map = useMap();
-
-  useEffect(() => {
-    if (!treeData) {
-      return;
-    }
-
-    const markerClusterGroup = L.markerClusterGroup({
-      removeOutsideVisibleBounds: true,
-      disableClusteringAtZoom: 16,
-    });
-
-    treeData.forEach(tree => {
-      const marker = L.marker([tree.Lat, tree.Lon], { icon: treeIcon });
-      marker.bindPopup(`<b>${tree.name}</b><br>${tree.description}`);
-      markerClusterGroup.addLayer(marker);
-    });
-
-    map.addLayer(markerClusterGroup);
-
-    // Clean up on unmount
-    return () => {
-      map.removeLayer(markerClusterGroup);
-    };
-  }, [map, treeData, treeIcon]);
-
-  return null;
-};
+import MarkerClusterGroupComponent from './MarkerClusterGroupComponent.jsx';
+import MapContext from './MapContext';
 
 
 const StadiaMap = ({ treeData }) => {
@@ -51,7 +18,11 @@ const StadiaMap = ({ treeData }) => {
     shadowSize: [41, 41]
   });
 
-  
+  const maxBounds = [
+    [50.56, 5.48], // Coordonnées du coin supérieur gauche
+    [50.72, 5.72]  // Coordonnées du coin inférieur droit
+  ];
+
   return (
     <MapContainer 
       center={[50.63373, 5.56749]} 
@@ -60,10 +31,7 @@ const StadiaMap = ({ treeData }) => {
       minZoom={13}
       maxZoom={16}
       zoomControl={false}
-      maxBounds={[
-        [50.56, 5.48], // Coordonnées du coin supérieur gauche
-        [50.72, 5.72]  // Coordonnées du coin inférieur droit
-      ]}
+      maxBounds={maxBounds}
       >
 
       <TileLayer
@@ -71,8 +39,9 @@ const StadiaMap = ({ treeData }) => {
         //attribution='&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
       />
 
-
-       <MarkerClusterGroupComponent treeData={treeData} treeIcon={treeIcon} />
+      <MapContext.Provider value={maxBounds}>
+        <MarkerClusterGroupComponent treeData={treeData} treeIcon={treeIcon} />
+      </MapContext.Provider>
 
     </MapContainer>
   );
