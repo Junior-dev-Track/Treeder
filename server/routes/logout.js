5db3b60e2c6ef5da5e4022fa70372da12f50c154
Kriidfel <g.mesureur@student.helmo.be>
2024-06-21 00:15:28 +0200

    refactor(logout): Modifing logout to use jwt token

--- server/routes/logout.js
+++ server/routes/logout.js
@@ -1,25 +1,11 @@
 const express = require('express');
-const DataBase = require("../model/DataBase");
-const bcrypt = require("bcrypt");
-const UserDB = require("../model/UserDB");
 const router = express.Router();
 
-
 router.post('/', (req, res) => {
-    req.session
-    if (req.session) {
-      req.session.destroy(err => {
-        if (err) {
-          res.status(400).send('Unable to log out')
-        } else {
-          res.send('Logout successful')
-        }
-      });
-    } else {
-        res.status(400).send('Not login')
-        res.end()
-        
-    }
+
+    res.clearCookie('jwt');
+    res.status(200).json({ message: 'Logged out' });
+
   })
 
 module.exports = router;
