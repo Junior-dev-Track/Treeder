954979b50d7b352349bb2ea06493f55db5011f53
Mathilde Po <mathilde@MacBook-Pro-de-Mathilde-2.local>
2024-06-25 16:56:20 +0200

    profile

--- client/src/pages/ProfilGamer.jsx
+++ client/src/pages/ProfilGamer.jsx
@@ -5,7 +5,7 @@ import SettingsGamer from './SettingsGamer';
 const ProfilGamer = ({ isOpen, setIsOpen }) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [selectedSkin, setSelectedSkin] = useState(1);
-  const [playerData, setPlayerData] = useState(null); 
+  
 
   const handleSkinSelect = (skinNumber) => {
     setSelectedSkin(skinNumber);
@@ -15,49 +15,27 @@ const ProfilGamer = ({ isOpen, setIsOpen }) => {
     setModalIsOpen(isOpen);
   }, [isOpen]);
 
-  useEffect(() => {
-    fetch('/profile', {
-      method: 'GET',
-      credentials: 'include',
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setPlayerData(data);
-      })
-      .catch((error) => console.error('Error:', error));
-  }, []);
-
   return (
     <div>
       <Modal isOpen={modalIsOpen} onRequestClose={() => {setModalIsOpen(false); setIsOpen(false);}}>
-      {playerData && (
-        <>
-          <img src="/path/to/avatar.png" alt="Avatar" />
-          <h2>Pseudo: {playerData.Pseudo}</h2>
-          <p>Classement: {playerData.classement}</p>
-          <p>Nombre d'arbres: {playerData.NbTrees}</p>
-          <p>Nombre de feuilles: {playerData.Leafs}</p>
-          <p>Nombre de locks: {playerData.Locks}</p>
-          <div>
-            <h3>Skins:</h3>
-            {[1, 2, 3, 4, 5].map((skinNumber) => (
-              <button
-                key={skinNumber}
-                onClick={() => handleSkinSelect(skinNumber)}
-                style={{ backgroundColor: selectedSkin === skinNumber ? 'blue' : 'grey' }}
-              >
-                Skin {skinNumber}
-              </button>
-            ))}
-          </div>
-        </>
-      )}
+        <img src="/path/to/avatar.png" alt="Avatar" />
+        <h2>Pseudo: Player1</h2>
+        <p>Classement: 1</p>
+        <p>Nombre d'arbres: 10</p>
+        <p>Nombre de feuilles: 100</p>
+        <p>Nombre de locks: 5</p>
+        <div>
+          <h3>Skins:</h3>
+          {[1, 2, 3, 4, 5].map((skinNumber) => (
+            <button
+              key={skinNumber}
+              onClick={() => handleSkinSelect(skinNumber)}
+              style={{ backgroundColor: selectedSkin === skinNumber ? 'blue' : 'grey' }}
+            >
+              Skin {skinNumber}
+            </button>
+          ))}
+        </div>
         <SettingsGamer />
         <button onClick={() => {setModalIsOpen(false); setIsOpen(false);}}>Close</button>
       </Modal>
