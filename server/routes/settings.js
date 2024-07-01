486d5c2d7ce3d228265cf420afbf87b4a1877993
“Lunoronti” <cacciatoremanon8@gmail.com>
2024-07-01 16:23:04 +0200

    settings

--- server/routes/settings.js
+++ server/routes/settings.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const UserDB = require('../model/UserDB');
 const DataBase = require ('../model/DataBase')
 const {authenticateToken} = require("../middleware/authenticateToken");
+const bcrypt = require("bcrypt");
 
 router.post('/', authenticateToken, async (req, res) => {
 
@@ -10,13 +11,33 @@ router.post('/', authenticateToken, async (req, res) => {
 
     let usersDB = new UserDB(new DataBase())
 
-    let users = await usersDB.updateUser(dataUser);
+    // data user pseudo et mail - trim sur les espaces blancs
+
+    dataUser.Pseudo = dataUser.Pseudo.trim();
+    dataUser.Mail = dataUser.Mail.trim();
+    dataUser.Password = dataUser.Password.trim();
 
     if (!dataUser.Pseudo || !dataUser.Password || !dataUser.Mail) {
         return res.status(400).send('Invalid data');
     }
 
+    // verifier la force du mdp
+    let regex = new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
+    if (!dataUser.Password.trim().match(regex)) {
+        return res.status(400).send('Invalid password');
+    }
+    else {
+        let salt = await bcrypt.genSalt(saltRounds);
+        dataUser.Password = await bcrypt.hash(dataUser.Password.trim(), salt);
+    }
 
+    // vérifier que l'user n'existe pas déjà
+    let user = await userDB.verifyUser(dataUser);
+    if (user.length > 0) {
+        return res.status(400).send('User already exists');
+    }
+
+    let users = await usersDB.updateUser(dataUser);
 
     res.status(200).send(users);
 });
