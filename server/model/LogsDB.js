5be0437123f2367d029ef5ef3ee6284be3ce87fa
Kriidfel <gillou8@hotmail.com>
2024-07-09 16:26:23 +0200

    refactor(logs): the logs get sorted by date the most recent first

--- server/model/LogsDB.js
+++ server/model/LogsDB.js
@@ -9,7 +9,8 @@ class LogsDB {
     }
 
     async getLogPlayer() {
-        return await this.dataBase.query(`SELECT l.Log, u.Pseudo, l.Date, l.Categorie FROM Logs as l JOIN Users as u ON l.User = u.IdUsers`)
+        //sort them by date the most recent first
+        return await this.dataBase.query(`SELECT * FROM Logs JOIN Users ON Logs.User = Users.IdUsers ORDER BY Logs.Date DESC`)
     }
 
     async getLogAdmin() {
