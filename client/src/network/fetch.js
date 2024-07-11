77668b8a5209f4583b1f57abd8044c0a3e43b618
“Lunoronti” <cacciatoremanon8@gmail.com>
2024-07-11 18:40:03 +0200

    error messages

---
+++ client/src/network/fetch.js
@@ -0,0 +1,12 @@
+export function fetchData() {
+    return fetch.apply(null, arguments).then(response => {
+        if (!response.ok) {
+            // create error object and reject if not a 2xx response code
+            let err = new Error("HTTP status code: " + response.status)
+            err.response = response
+            err.status = response.status
+            throw err
+        }
+        return response
+    })
+}
